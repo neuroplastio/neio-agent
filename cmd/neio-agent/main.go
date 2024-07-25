5d4b05fb722170a2f9a849d1c2b10720edb3e6dd
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2024-07-25 21:01:39 +0200

    chore: format file

--- cmd/neio-agent/main.go
+++ cmd/neio-agent/main.go
@@ -10,15 +10,14 @@ import (
 	"github.com/neuroplastio/neuroplastio/pkg/agent/agentcli"
 )
 
-
 func main() {
 	ctx := context.Background()
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 	defer stop()
+
 	err := agentcli.Main(ctx, os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 }
-
